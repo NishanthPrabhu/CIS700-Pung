32b7496f72382ed4834230d53ed92eb57c6eae06
Tilak Raj Singh <tilak72@gmail.com>
2019-04-19 22:32:17 -0400

    sending messages to server

--- src/client.cc
+++ src/client.cc
@@ -5,6 +5,8 @@
 #include "client.h"
 #include "rpc/server.h"
 
+#define MESSAGE_LEN 256
+
 static void init(void)
 {
     if (sodium_init() != 0)
@@ -70,13 +72,68 @@ void register_client(rpc::client *client, int id, string ip, unsigned char* publ
 	client->call("set_client_key", id, ip, get_hex(public_key, crypto_kx_PUBLICKEYBYTES));
 }
 
+void send_message()
+{
+	if(!msgs.empty())
+	{
+		string message = msgs.front();
+		msgs.pop();
+		
+		int cur_length = message.length();
+		cur_length = to_string(cur_length).length()+cur_length+1;
+		
+		if(cur_length > MESSAGE_LEN)
+		{
+			cout << "Packed message length greater than MAX_LENGTH\n";
+			return;
+		}
+		
+		int remaining = MESSAGE_LEN - cur_length;
+		
+		message = to_string(cur_length)+"|"+message+string(remaining, ' ');
+		
+		//cout << "Message is \n" << message << "******\n"; 
+	
+		unsigned char nonce[crypto_secretbox_NONCEBYTES];
+		unsigned char ciphertext[crypto_secretbox_MACBYTES + MESSAGE_LEN];
+
+		randombytes_buf(nonce, sizeof nonce);
+		crypto_secretbox_easy(ciphertext, (const unsigned char*)message.c_str(), message.length(), nonce, peer.get_key_e());
+		
+		string ciphertext_s = get_hex(ciphertext, crypto_secretbox_MACBYTES + MESSAGE_LEN);
+	
+		//cout << "Ciphertext is " << get_hex(ciphertext, crypto_secretbox_MACBYTES + MESSAGE_LEN) << "\n";
+		
+		client.client->call("store_message", round.get_label_s(), ciphertext_s);
+		
+		
+		//TODO move lower part when retrieving messages
+		
+		if (sodium_hex2bin(ciphertext, crypto_secretbox_MACBYTES + MESSAGE_LEN, ciphertext_s.c_str(), ciphertext_s.length(), NULL, NULL, NULL) == -1)
+		{
+			cout << "Aborting. Peer public key corrupt\n";
+		    //return false;
+		}
+
+		unsigned char decrypted[MESSAGE_LEN];
+		if (crypto_secretbox_open_easy(decrypted, ciphertext, crypto_secretbox_MACBYTES + MESSAGE_LEN, nonce, peer.get_key_e()) != 0) {
+			/* message forged! */
+		}
+		
+		cout << "Decrypted is " << string((const char *)decrypted, MESSAGE_LEN) << "\n";		
+	}
+}
+
 void initialize_new_round(std::string round_id) {
 
     round.set_round_id(round_id);
-    //cout << "New round: " << round.get_round_id() << std::endl;
+    cout << "New round: " << round.get_round_id() << std::endl;
     
     if(peer_joined)
+    {
     	create_round_labels();
+    	send_message();
+    }
 }
 
 void create_round_labels()
@@ -84,13 +141,13 @@ void create_round_labels()
 	string s_str = round.get_round_id()+"||"+to_string(peer.get_peer_id());
 	string r_str = round.get_round_id()+"||"+to_string(client.get_id());
 	
-	unsigned char hash[crypto_auth_hmacsha512_BYTES];
+	unsigned char hash[crypto_auth_hmacsha256_BYTES];
 	
-	crypto_auth_hmacsha512(hash, (const unsigned char *)s_str.c_str(), s_str.length(), peer.get_key_l());
+	crypto_auth_hmacsha256(hash, (const unsigned char *)s_str.c_str(), s_str.length(), peer.get_key_l());
 	round.set_label_s(get_hex(hash, sizeof hash));
 	//cout << "label_s : " << round.get_label_s() << "\n";
 	
-	crypto_auth_hmacsha512(hash, (const unsigned char *)r_str.c_str(), r_str.length(), peer.get_key_l());
+	crypto_auth_hmacsha256(hash, (const unsigned char *)r_str.c_str(), r_str.length(), peer.get_key_l());
 	round.set_label_r(get_hex(hash, sizeof hash));
 	//cout << "label_r : " << round.get_label_r() << "\n";;
 	
@@ -251,7 +308,7 @@ int main(int argc, char **argv) {
 								cin.get();
 								if(create_comm_keys(peer_id))
 								{
-									cout << "Communication started with peer \n" << peer_id;
+									cout << "Communication started with peer " << peer_id << "\n";
 									cout << "Type /help anytime to check other command options\n";
 								}
 								break;
