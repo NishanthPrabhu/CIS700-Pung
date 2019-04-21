21c80fb081ca226d770126b2ca6b4a3272773d06
Nishanth Prabhu <nishanthprabhu94@gmail.com>
2019-04-21 18:04:46 +0000

    clean

--- src/slave_callbacks.h
+++ src/slave_callbacks.h
@@ -11,7 +11,7 @@
 
 void initialize_new_round(std::string round_id);
 void set_client_public_key(int client_id, std::string const& client_ip, std::vector<unsigned char> const&);
-void set_and_propagate_client_public_key(int client_id, std::string const& client_ip, std::string const &publickey);
+void set_and_propagate_client_public_key(int client_id, std::string const& client_ip, std::vector<unsigned char> const &publickey);
 std::vector<unsigned char> get_public_key(int client_id);
 void store_and_propagate_message(int index, std::string label, std::string message);
 void store_message(int index, std::string label, std::string message);
